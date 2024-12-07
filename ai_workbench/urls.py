c47d33719773a641aa2912aaa82a84054bcdc6dc
rompio <ramonpolanski@gmail.com>
2024-12-07 15:02:28 +0100

    black .

--- ai_workbench/urls.py
+++ ai_workbench/urls.py
@@ -3,8 +3,12 @@ from django.urls import path, include
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path("admin/", admin.site.urls),          # Matches /admin/
-    path("", include("home.urls")),      # Matches /home/ and forwards to home/urls.py
-    path("users/", include("users.urls")),    # Matches /users/ and forwards to users/urls.py
+    path("admin/", admin.site.urls),  # Matches /admin/
+    path(
+        "", include("home.urls")
+    ),  # Matches /home/ and forwards to home/urls.py
+    path(
+        "users/", include("users.urls")
+    ),  # Matches /users/ and forwards to users/urls.py
     path("job_hunter/", include("job_hunter.urls")),  # Matches /job_hunter/
 ]
