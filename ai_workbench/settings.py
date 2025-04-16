0a5acd521916fdaa77244ccb85e2f95d133200e6
rompio <ramonpolanski@gmail.com>
2025-04-16 17:26:16 +0200

    allowe local host

--- ai_workbench/settings.py
+++ ai_workbench/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["rompion-workbench-14980d8a0750.herokuapp.com"]
+ALLOWED_HOSTS = ["rompion-workbench-14980d8a0750.herokuapp.com", '127.0.0.1']
 
 
 # Application definition
