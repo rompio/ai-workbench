455de3c42c9c46be0ea5744342660a3e5b6f78bb
rompio <ramonpolanski@gmail.com>
2025-04-16 17:06:42 +0200

    enable local sqlite3.db in settings

--- ai_workbench/settings.py
+++ ai_workbench/settings.py
@@ -69,9 +69,16 @@ WSGI_APPLICATION = "ai_workbench.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+import dj_database_url
+
+# Fallback auf SQLite, falls keine DATABASE_URL gesetzt ist
 DATABASES = {
-    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    "default": dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
+    )
 }
+
 
 
 # Password validation
