9bbf277d7864d20cd399a1ca626a7fb5c649979c
rompio <ramonpolanski@gmail.com>
2025-04-16 16:56:09 +0200

    black settings

--- ai_workbench/settings.py
+++ ai_workbench/settings.py
@@ -17,8 +17,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rompion-workbench-14980d8a0750.herokuapp.com']
-
+ALLOWED_HOSTS = ["rompion-workbench-14980d8a0750.herokuapp.com"]
 
 
 # Application definition
@@ -43,7 +42,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "ai_workbench.urls"
@@ -71,12 +70,8 @@ WSGI_APPLICATION = "ai_workbench.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
-
-
 
 
 # Password validation
@@ -115,8 +110,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
