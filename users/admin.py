d11211e799921579f6e15e2b9b5025af4101e725
rompio <ramonpolanski@gmail.com>
2025-04-16 17:28:34 +0200

    change users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Tool
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'job_title', 'role', 'is_verified', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        ("Zusätzliche Felder", {
+            "fields": (
+                "job_title",
+                "tools",
+                "role",
+                "is_verified",
+            ),
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Zusätzliche Felder", {
+            "fields": (
+                "job_title",
+                "tools",
+                "role",
+                "is_verified",
+            ),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Tool)
