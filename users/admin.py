4e0afa49d53c3aabc750ae6a348606ca0654a57e
rompio <ramonpolanski@gmail.com>
2025-04-16 17:38:00 +0200

    black

--- users/admin.py
+++ users/admin.py
@@ -2,29 +2,44 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Tool
 
+
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['username', 'email', 'job_title', 'role', 'is_verified', 'is_staff']
+    list_display = [
+        "username",
+        "email",
+        "job_title",
+        "role",
+        "is_verified",
+        "is_staff",
+    ]
     fieldsets = UserAdmin.fieldsets + (
-        ("Zusätzliche Felder", {
-            "fields": (
-                "job_title",
-                "tools",
-                "role",
-                "is_verified",
-            ),
-        }),
+        (
+            "Zusätzliche Felder",
+            {
+                "fields": (
+                    "job_title",
+                    "tools",
+                    "role",
+                    "is_verified",
+                ),
+            },
+        ),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ("Zusätzliche Felder", {
-            "fields": (
-                "job_title",
-                "tools",
-                "role",
-                "is_verified",
-            ),
-        }),
+        (
+            "Zusätzliche Felder",
+            {
+                "fields": (
+                    "job_title",
+                    "tools",
+                    "role",
+                    "is_verified",
+                ),
+            },
+        ),
     )
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Tool)
