b6e6e572283d9f068223864eef04c5c724115c91
rompio <ramonpolanski@gmail.com>
2025-05-04 17:58:15 +0200

    update database connection for local and remote

--- ai_workbench/settings.py
+++ ai_workbench/settings.py
@@ -85,16 +85,23 @@ import dj_database_url
 #         default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
 #     )
 # }
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "workbench",
-        "USER": "roman",
-        "PASSWORD": "1984",
-        "HOST": "localhost",
-        "PORT": "5432",
+USE_DJ_DATABASE_URL = os.getenv('USE_DJ_DATABASE_URL', 'true').lower() == 'true'
+
+if USE_DJ_DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST', 'localhost'),
+            'PORT': os.getenv('DB_PORT', '5432'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
