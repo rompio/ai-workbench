3295a151a44baced6418ae50be4cabf8ccadd851
rompio <ramonpolanski@gmail.com>
2025-05-05 16:15:43 +0200

    nchange header in base.html marking active app

--- health_tracker/views.py
+++ health_tracker/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def dashboard(request):
     return render(request, 'health_tracker/dashboard.html')
